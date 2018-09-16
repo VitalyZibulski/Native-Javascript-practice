59febe2cc37a373a40cdddd5c886a90fde04d169
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-16 16:05:16 +0300

    eventListener

--- js/plugin.js
+++ js/plugin.js
@@ -33,3 +33,18 @@ function addList(list){
 }
 
 generateList(tasks);
+
+let btn = document.querySelector('.clear-btn');
+
+function onLick(e){
+	console.log('click 1', e);
+}
+
+function onLick2(e){
+	console.log('click 2', e);
+}
+
+btn.addEventListener("click", onClick);
+btn.addEventListener("click", onClick2);
+
+btn.removeEventListener("click",onClick2);
