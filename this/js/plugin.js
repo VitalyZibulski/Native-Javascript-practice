04733536f273a029912b257c88d6c9d59da7f6ab
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-19 00:07:11 +0300

    this object method

--- this/js/plugin.js
+++ this/js/plugin.js
@@ -1,12 +1,25 @@
+function getPrice(){
+	console.log(this);
+	return `this product cost ${this.price}`;
+}
+
 let intel = {
 	name:'Intel i7 3.5 Ghz',
 	price:100,
 	discount:10,
 	count:50,
-	getPrice: function(){
-		console.log(this);
-		return `this product cost ${intel.price}`;
-	}
+	getPrice: getPrice
+};
+
+let amd = {
+	name:'AMD 3.5 Ghz',
+	price:100,
+	discount:10,
+	count:50,
+	getPrice: getPrice
 };
 
 intel.getPrice();
+amd.getPrice();
+getPrice();
+
