cc779d6cd9e3706d13a6950ff9cc35170b136953
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-20 10:40:53 +0300

    work with Date

---
+++ Date/js/plugin.js
@@ -0,0 +1,15 @@
+// let date = new Date();
+
+// let methods;
+
+// 	methods = date.getSeconds();
+
+// console.log(methods);
+
+let date = new Date();
+
+let methods;
+
+date.setMinutes(date.getMinutes() + 10);
+
+console.log(date);
