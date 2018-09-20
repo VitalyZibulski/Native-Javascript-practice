620769bdb4233328f1508491a7a1824ee6017ce4
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-20 13:52:39 +0300

    add Event

--- Date/js/plugin.js
+++ Date/js/plugin.js
@@ -3,10 +3,17 @@ const buttons = document.querySelectorAll('[data-time]');
 const timer = (function(){
 	let countDown,
 		timerDisplay,
-		endTime
+		endTime,
+		alarmSound
 
 	function init(settings){
+		console.log(settings);
+		timerDisplay = document.querySelector(settings.timeLeftSelector);
+		endTime = document.querySelector(settings.timeEndSelector);
 
+		if(settings.alarmSound){
+			alarmSound = new Audio(settings.alarmSound);
+		}
 	}
 
 	function start(seconds){
@@ -43,3 +50,12 @@ timer.init({
 	timeEndSelector:'.display__end-time',
 	alarmSound:'audio/bell.mp3'
 })
+
+//Start timer by click
+function startTimer(e){
+	const seconds = parseInt(this.dataset.time);
+	//console.log(seconds);
+	timerStart(seconds);
+}
+
+buttons.forEach(btn => btn.addEventListener('click',startTimer));
