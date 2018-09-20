ad786fd270926b05727ce1ccc7a4c02fc284b736
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-20 18:34:57 +0300

    ES6 create class

--- Class/js/plugin.js
+++ Class/js/plugin.js
@@ -1,20 +1,34 @@
-function Persone(firstname, lastname, age){
-	this.firstname = firstname;
-	this.lastname = lastname;
+// function Persone(firstname, lastname, age){
+// 	this.firstname = firstname;
+// 	this.lastname = lastname;
+// }
+
+// let user= new Persone('user', 'user1', 29);
+
+// function Customer(firstname,lastname,phone,membership){
+// 	Persone.call(this,firstname,lastname);
+
+// 	this.phone = phone;
+// 	this.membership = membership;
+// }
+
+// Customer.prototype = Object.create(Persone.prototype);
+
+// Customer.prototype.constructor = Customer;
+
+// let customer1 = new Customer('user2','customer','1111', 'Standart');
+// console.log(customer1);
+
+//classes ES6
+class Persone{
+	constructor(firstName, lastName){
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	greeting(){
+		return `${firstname}${lastname}`;
+	}
 }
 
-let user= new Persone('user', 'user1', 29);
-
-function Customer(firstname,lastname,phone,membership){
-	Persone.call(this,firstname,lastname);
-
-	this.phone = phone;
-	this.membership = membership;
-}
-
-Customer.prototype = Object.create(Persone.prototype);
-
-Customer.prototype.constructor = Customer;
-
-let customer1 = new Customer('user2','customer','1111', 'Standart');
-console.log(customer1);
+const user = new Person ('user', 'user1');
