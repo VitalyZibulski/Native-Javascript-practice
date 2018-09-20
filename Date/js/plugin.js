19331ebcef5ea3c665bc02e51f0d6af039cf11c5
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-20 14:15:16 +0300

    display end time

--- Date/js/plugin.js
+++ Date/js/plugin.js
@@ -17,7 +17,13 @@ const timer = (function(){
 	}
 
 	function start(seconds){
+		if(!timerDisplay || !endTime) return console.log('Please init module first');
+		if(!seconds || typeof seconds !== 'number') return console.log('Please provide seconds');
 
+		const now = Date.now();
+		const then = now + seconds * 1000;
+
+		displayEndTime(then);
 	}
 
 	function displayTimeLeft(seconds){
@@ -25,7 +31,11 @@ const timer = (function(){
 	}
 
 	function displayEndTime(timestamp){
+		const end = new Date(timestamp);
+		const hour = end.getHours();
+		const minutes = end.getMinutes();
 
+		endTime.textContent = `Be back at ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 	}
 
 	function stop(){
@@ -55,7 +65,7 @@ timer.init({
 function startTimer(e){
 	const seconds = parseInt(this.dataset.time);
 	//console.log(seconds);
-	timerStart(seconds);
+	timer.start(seconds);
 }
 
 buttons.forEach(btn => btn.addEventListener('click',startTimer));
