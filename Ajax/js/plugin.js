b5121c23ccea42450357a8a8cd76cd540be87067
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-22 21:55:40 +0300

    load

--- Ajax/js/plugin.js
+++ Ajax/js/plugin.js
@@ -1,11 +1,15 @@
 const xhr = new XMLHttpRequest();
 
-xhr.addEventListener('readystatechange', function(e){
-	console.log(xhr.readyState);
-	if(xhr.readyState === 4){
-		console.log(xhr.responseText);
-	}
-})
+// xhr.addEventListener('readystatechange', function(e){
+// 	console.log(xhr.readyState);
+// 	if(xhr.readyState === 4){
+// 		console.log(xhr.responseText);
+// 	}
+// })
+
+xhr.addEventListener('load', function (e){
+	console.log(xhr.responseText);
+});
 
 xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
 
