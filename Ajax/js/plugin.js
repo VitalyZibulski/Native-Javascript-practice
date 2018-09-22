86501b0864f5c5a652bf3dcb00130f31442fb071
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-22 21:53:54 +0300

    ready state

--- Ajax/js/plugin.js
+++ Ajax/js/plugin.js
@@ -1,3 +1,13 @@
 const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('readystatechange', function(e){
+	console.log(xhr.readyState);
+	if(xhr.readyState === 4){
+		console.log(xhr.responseText);
+	}
+})
+
 xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
+
 xhr.send();
+
