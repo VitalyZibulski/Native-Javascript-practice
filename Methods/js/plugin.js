1adfbb76880fb9336416628d392eeddf2d89cd0a
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-23 11:50:02 +0300

    method filter

---
+++ Methods/js/plugin.js
@@ -0,0 +1,34 @@
+// forEach, filter, map, every, some, reduce
+let numbers = [10,20,-10,2,-5];
+let users = [
+	{
+		name:'user',
+		age: 26,
+		gender:'male'
+	},
+	{
+		name:'user1',
+		age: 28,
+		gender:'female'
+	},
+	{
+		name:'user2',
+		age: 29,
+		gender:'male'
+	}
+]
+
+//Filter
+
+let res = numbers.filter((item, i, arr) => {
+	// console.log(item,i,arr);
+	return item >= 0;
+})
+
+// res = users.filter(user => {
+// 	return user.gender === 'male';
+// });
+
+res = users.filter(user => user.gender === 'male');
+
+// console.log(res);
