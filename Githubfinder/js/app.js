a4daf5b9450490c2ba3fe3fbb6d49c7838e56023
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-09-27 11:27:35 +0300

    add lister keyup

--- Githubfinder/js/app.js
+++ Githubfinder/js/app.js
@@ -1,1 +1,10 @@
-app.js
+// Init search input
+const searchInput = document.getElementById('searchUser');
+
+//Add Event Listener
+searchInput.addEventListener('keyup',(e) => {
+	// Get input TEext
+	const userText = e.target.value;
+
+	console.log(userText);
+});
