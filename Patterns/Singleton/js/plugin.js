34af157d660867607c8457684f468540474b6fb1
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-02 18:28:04 +0300

    pattern singleton

--- Patterns/Singleton/js/plugin.js
+++ Patterns/Singleton/js/plugin.js
@@ -8,3 +8,33 @@ const Module = (function(){
 })();
 
 // ES6 - exist modules; export and import;
+
+// Singleton
+
+const counterModule = (function() {
+	let instance,
+		counter = 0;
+
+	const getCounter = function() {
+		return counter;
+	}
+
+	const increaseCounter = function() {
+		counter++;
+	}
+
+	const createInstance = function() {
+		return {
+			getCounter,
+			increaseCounter
+		}
+	}
+
+	return {
+		getInstance: function() {
+			return instance || instance = (createInstance());
+		}
+	}
+}());
+
+const counter1 = counterModule.getInstance();
