523b8760fb3807a2cb781bca02dc1ba8651837ef
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-02 23:22:19 +0300

    pattern factory

--- Patterns/Singleton/js/plugin.js
+++ Patterns/Singleton/js/plugin.js
@@ -32,9 +32,63 @@ const counterModule = (function() {
 
 	return {
 		getInstance: function() {
-			return instance || instance = (createInstance());
+			return instance || (instance = createInstance());
 		}
 	}
 }());
 
 const counter1 = counterModule.getInstance();
+
+// Factory
+class SimpleMemberShip{
+	constructor(name) {
+		this.name = name;
+		this.cost = '$5';
+	}
+}
+
+class StandartMemberShip{
+	constructor(name) {
+		this.name = name;
+		this.cost = '$15';
+	}
+}
+
+class SuperMemberShip{
+	constructor(name) {
+		this.name = name;
+		this.cost = '$25';
+	}
+}
+
+function MemberFactory(){
+	this.createMember = function(name, type) {
+		let member;
+
+		if(type === 'simple') {
+			member = new SimpleMemberShip(name);
+		} else if (type === 'standart') {
+			member = new StandartMemberShip(name);
+		} else if (type === 'super') {
+			member = new SuperMemberShip(name);
+		}
+
+		member.type = type;
+
+		member.define = function() {
+			return `${this.name}(${this.type}) : ${this.cost}`;
+		}
+
+		return member;
+	}
+}
+
+const members = [];
+
+const factory = new MemberFactory();
+
+factory.createMember('User','super');
+
+members.push(factory.createMember('User','standart'));
+
+members.push(factory.createMember('User1','simple'));
